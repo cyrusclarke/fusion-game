62937bef9a03eedc0a99ad47dc65496bf71f1eaf
cyb0rg <cyrusjclarke@gmail.com>
2019-04-08 13:25:38 +0200

    updated click through

---
+++ skeleton/js/config.js
@@ -0,0 +1,4 @@
+var config = {
+  MY_KEY : 'AIzaSyBusXNo18oKpHFdbZRo47gkw8ziWA_9kq0',
+  SENDER_ID : '582725448081',
+}
--- skeleton/js/firebase.js
+++ skeleton/js/firebase.js
@@ -1,10 +1,15 @@
+var mykey = config.MY_KEY;
+var secretkey = config.SENDER_ID;
+
+
+
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyBusXNo18oKpHFdbZRo47gkw8ziWA_9kq0",
+	apiKey: mykey,
 	authDomain: "fusionloop.firebaseapp.com",
 	databaseURL: "https://fusionloop.firebaseio.com",
 	projectId: "fusionloop",
 	storageBucket: "fusionloop.appspot.com",
-	messagingSenderId: "582725448081"
+	messagingSenderId: secretkey
 };
 firebase.initializeApp(config);
